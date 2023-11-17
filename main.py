39d04949f4e96736a2d136a70fc45dde02754471
NVishnevskaya <140640449+NVishnevskaya@users.noreply.github.com>
2023-11-17 17:30:34 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+import sys
+
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5 import uic
+from random import randrange
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.initUI()
+
+    def initUI(self):
+        self.do_paint = False
+        self.btn.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(255, 251, 0))
+        d = randrange(10, 300)
+        qp.drawEllipse(50, 70, d, d)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
