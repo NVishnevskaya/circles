b0d1564fce3dbc308dd322dc4c70509c36c29675
NVishnevskaya <140640449+NVishnevskaya@users.noreply.github.com>
2023-11-19 09:28:42 +0300

    The third task
    I've changed main.py due to the task: added class Interface, generation of random circle's color.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import io
 
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QMainWindow, QApplication
@@ -6,10 +7,64 @@ from PyQt5 import uic
 from random import randrange
 
 
-class Example(QMainWindow):
+class Interface:
     def __init__(self):
+        self.template = """<?xml version="1.0" encoding="UTF-8"?>
+<ui version="4.0">
+ <class>MainWindow</class>
+ <widget class="QMainWindow" name="MainWindow">
+  <property name="geometry">
+   <rect>
+    <x>0</x>
+    <y>0</y>
+    <width>740</width>
+    <height>408</height>
+   </rect>
+  </property>
+  <property name="windowTitle">
+   <string>MainWindow</string>
+  </property>
+  <widget class="QWidget" name="centralwidget">
+   <widget class="QPushButton" name="btn">
+    <property name="geometry">
+     <rect>
+      <x>10</x>
+      <y>20</y>
+      <width>201</width>
+      <height>41</height>
+     </rect>
+    </property>
+    <property name="text">
+     <string>Нарисовать окружности</string>
+    </property>
+   </widget>
+  </widget>
+  <widget class="QMenuBar" name="menubar">
+   <property name="geometry">
+    <rect>
+     <x>0</x>
+     <y>0</y>
+     <width>740</width>
+     <height>21</height>
+    </rect>
+   </property>
+  </widget>
+  <widget class="QStatusBar" name="statusbar"/>
+ </widget>
+ <resources/>
+ <connections/>
+</ui>
+        """
+
+    def get_design(self):
+        return self.template
+
+
+class Example(QMainWindow):
+    def __init__(self, interface=Interface()):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        f = io.StringIO(interface.get_design())
+        uic.loadUi(f, self)
         self.initUI()
 
     def initUI(self):
@@ -29,11 +84,14 @@ class Example(QMainWindow):
         self.update()
 
     def draw_flag(self, qp):
-        qp.setBrush(QColor(255, 251, 0))
-        for _ in range(randrange(1, 12)):
-            d = randrange(10, 300)
-            x = randrange(50, 401)
-            y = randrange(70, 351)
+        for _ in range(randrange(1, 31)):
+            r = randrange(0, 256)
+            g = randrange(0, 256)
+            b = randrange(0, 256)
+            qp.setBrush(QColor(r, g, b))
+            d = randrange(1, 351)
+            x = randrange(0, 401)
+            y = randrange(60, 351)
             qp.drawEllipse(x, y, d, d)
 
 
